35a2e631898a490f1f5de9c8f6c1e7cd0fc72959
imranchaibi <71709538+imranchaibi@users.noreply.github.com>
2020-09-28 13:10:20 +0200

    Add files via upload

---
+++ live coding 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a = 22;
+    int b = 10;
+    if (a > 20)
+    {
+        printf ("eroor\n");
+    }
+    else if (a > b)
+    {
+        printf ("GOOD\n");
+    }
+    else if (a < b)
+    {
+        printf ("non-admis\n");
+    }
+    else if(a = b)
+    {
+        printf ("accebtable\n");
+    }
+
+    return 0;
+}
